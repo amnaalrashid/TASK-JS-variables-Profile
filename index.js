09cc6b4efa940f63cc4b83268392e11842c48a20
amnaalrashid <amna.alrashid@gmail.com>
2024-08-05 20:28:00 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,10 +11,17 @@
 
 // Part 1 answer 👇🏻 ...
 
-
-
-
-
+let fullName = "Amnah";
+let yearOfBirth = "1995";
+let hobby = "Reading";
+let funFact = "I enjoy cooking!";
+const image =
+  "https://us.123rf.com/450wm/watcartoon/watcartoon2204/watcartoon220400021/186469783-children-boy-thinking-idea-and-books-cute-kid-image-in-classroom-in-circle-icon.jpg?ver=6";
+console.log(fullName);
+console.log(yearOfBirth);
+console.log(hobby);
+console.log(funFact);
+console.log(image);
 
 /**
  * Part 2: String Interpolation
@@ -24,17 +31,14 @@
  * 1. fullNameString      -> assign it to: My name is {fullName}
  * 2. yearOfBirthString   -> assign it to: I am {YOUR_AGE}, and make sure you calculate your age from your year of birth
  * 3. hobbyString         -> assign it to: My hobby is {YOUR_HOBBY}
- * 
+ *
  * Uncomment the following lines and start interpolating.
  * You can uncomment a line by removing the // at the start
  */
 
-// const fullNameString = `My name is ${}`;
-// const yearOfBirthString =
-// const hobbyString =
-
-
-
+const fullNameString = `My name is ${"Amnah Alrashed"}`;
+const yearOfBirthString = `My year of birth ${1995}`;
+const hobbyString = `My hobby ${"Reading"}`;
 
 /**
  * Part 3: Re-assignment
